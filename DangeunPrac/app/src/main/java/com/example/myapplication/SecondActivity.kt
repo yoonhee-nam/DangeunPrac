237dc2710dd2d7d7a4beffdc366ec8114721a53d
yoonhee~ <flvps123@gmail.com>
2023-08-29 20:51:03 +0900

    [FEAT] : ⚙️ 좋아요데이터추가 / 스낵바추가 / 단위변경

--- DangeunPrac/app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ DangeunPrac/app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.databinding.ActivitySecondBinding
+import com.google.android.material.snackbar.Snackbar
 
 class SecondActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,8 +15,14 @@ class SecondActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        binding.arrow.setOnClickListener {
+            exit()
+        }
+
 
         val clickItem = intent.getParcelableExtra<MyItem>("clickItem")
+
+
 
         if (clickItem != null) {
 
@@ -24,6 +33,32 @@ class SecondActivity : AppCompatActivity() {
             binding.textView7.text = clickItem.aReason
             binding.textView8.text = clickItem.aPrice
 
+            var heart = clickItem?.heart == true
+
+            binding.heart.setImageResource(if(heart) {R.drawable.fillheart}else{R.drawable.empty_heart})
+
+            binding.heart.setOnClickListener {
+                if(!heart){
+                    binding.heart.setImageResource(R.drawable.fillheart)
+                    Snackbar.make(binding.constraintLayout, "관심목록에 추가됐습니다.", Snackbar.LENGTH_SHORT).show()
+                    heart = true
+                } else {
+                    binding.heart.setImageResource(R.drawable.empty_heart)
+                    heart = false
+                }
+            }
         }
+    }
+
+    fun exit() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra("clickItem", intent)
+            putExtra("heart", intent)
+        }
+        setResult(RESULT_OK, intent)
+        if (!isFinishing) finish()
+    }
+    override fun onBackPressed() {
+        exit()
     }
 }
